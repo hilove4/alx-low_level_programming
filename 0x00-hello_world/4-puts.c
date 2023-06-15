57c71112763497479ffb536b18c59ed06ae5346d
hilove4 <hay.wel2001@gmail.com>
2023-06-15 15:34:09 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/**
- *  This is a program to put 
- * Return : 0 (Succes) 
- */
-int main(void) {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
-}
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
