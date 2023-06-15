416cd40f15531c98c4073e6c99cd974f43526293
hilove4 <hay.wel2001@gmail.com>
2023-06-15 15:50:14 -0700

    puts

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("You are not allowed to use the function puts,\n");
-	printf("but your program can still create a masterpiece.\n");
-	printf("With proper grammar, it becomes a piece of art,\n");
-	printf("followed by a new line.\n");
-
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
-
