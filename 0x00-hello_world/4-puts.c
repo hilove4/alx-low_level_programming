b12b422ef98ded5f33e36ffe04ada565313976d7
hilove4 <hay.wel2001@gmail.com>
2023-06-15 15:17:12 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/** This is a program to put 
+ * 0 (Succes)
+ * main- This program wil put the file 
+ */
+int main() {
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
+
