576db7a1aa542eaf4d9bfee8213d5fe328757225
hilove4 <hay.wel2001@gmail.com>
2023-06-15 15:40:19 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("You are not allowed to use the function puts,\n");
+	printf("but your program can still create a masterpiece.\n");
+	printf("With proper grammar, it becomes a piece of art,\n");
+	printf("followed by a new line.\n");
+
+	return (0);
+}
+
