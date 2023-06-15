520329db100173eed431a0bba1e7fdada0a9ff0f
hilove4 <hay.wel2001@gmail.com>
2023-06-15 16:47:31 -0700

    main

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar,"
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, "
 
 		"2015-10-19\n", stdout);
 
