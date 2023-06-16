c912cc3ac571b5343e9ad2f2fb1ea2f6378edaeb
hilove4 <hay.wel2001@gmail.com>
2023-06-15 17:01:40 -0700

    fput

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point of the program
  *
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, "
+	fput("and that piece of art is useful\" - Dora Korpar, "
 
 		"2015-10-19\n", stdout);
 
